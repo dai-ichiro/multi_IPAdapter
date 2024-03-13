b9cb9957c4c7194335242edc6c3fe2da486fb148
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2024-03-13 17:02:12 +0900

    update run.py

--- run.py
+++ run.py
@@ -1,37 +1,52 @@
 import torch
 from diffusers import AutoPipelineForText2Image, DPMSolverMultistepScheduler
 import argparse
+import itertools
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--model',
+    "--model",
     type=str,
     required=True,
     help="sdxl model",
 )
 parser.add_argument(
-    '--plusface',
+    "--plusface",
     type=str,
     required=True,
     help="embeddings for plusface"
 )
 parser.add_argument(
-    '--faceid',
+    "--plusface_scale",
+    type=float,
+    help="scale of ip-apdapter plusface"
+)
+parser.add_argument(
+    "--faceid",
     type=str,
     required=True,
     help="embeddings for facdid"
 )
 parser.add_argument(
-    '--style',
+    "--faceid_scale",
+    type=float,
+    help="scale of ip-apdapter faceid"
+)
+parser.add_argument(
+    "--style",
     type=str,
     help="embeddings for style",
 )
+parser.add_argument(
+    "--style_scale",
+    type=float,
+    help="scale of style"
+)
 args = parser.parse_args()
 
-model_id = args.model
-
 pipeline = AutoPipelineForText2Image.from_pretrained(
-    model_id,
+    args.model,
     torch_dtype=torch.float16,
     variant="fp16"
 ).to("cuda")
@@ -44,7 +59,14 @@ pipeline.scheduler = DPMSolverMultistepScheduler(
         algorithm_type="sde-dpmsolver++",
         use_karras_sigmas=True
 )
+
+plusface_scale_list = [args.plusface_scale] if args.plusface_scale else [0.1, 0.3, 0.5]
+faceid_scale_list = [args.faceid_scale] if args.faceid_scale else [0.3, 0.5, 0.7, 0.9]
+
+Path("results").mkdir(exist_ok=True)
+
 if args.style:
+    style_scale_list = [args.style_scale] if args.style_scale else [0.3, 0.5]
     # text2image with 3 image embeddings
     pipeline.load_ip_adapter(
         ["IP-Adapter", "IP-Adapter", "IP-Adapter-FaceID"],
@@ -56,26 +78,28 @@ if args.style:
         ],
         image_encoder_folder=None
     )
-    pipeline.set_ip_adapter_scale([0.5, 0.5, 0.5])
 
     image_embeds_plusface = torch.load(args.plusface)
     image_embeds_faceid = torch.load(args.faceid)
     image_embeds_style = torch.load(args.style)
     image_embeds = [image_embeds_style[0], image_embeds_plusface[0], image_embeds_faceid[0]]
 
-    image = pipeline(
-        prompt="a woman",
-        ip_adapter_image_embeds=image_embeds,
-        negative_prompt="monochrome, lowres, bad anatomy, worst quality, low quality", 
-        num_inference_steps=50,
-        num_images_per_prompt=1,
-        guidance_scale = 7.5,
-        width=1024,
-        height=1024, 
-        generator=torch.Generator(device="cpu").manual_seed(0)
-    ).images[0]
+    for (style_scale, plusface_scale, faceid_scale) in itertools.product(style_scale_list, plusface_scale_list, faceid_scale_list):
+        pipeline.set_ip_adapter_scale([style_scale, plusface_scale, faceid_scale])
+        image = pipeline(
+            prompt="a woman",
+            ip_adapter_image_embeds=image_embeds,
+            negative_prompt="monochrome, lowres, bad anatomy, worst quality, low quality", 
+            num_inference_steps=50,
+            num_images_per_prompt=1,
+            guidance_scale = 7.5,
+            width=1024,
+            height=1024, 
+            generator=torch.Generator(device="cpu").manual_seed(0)
+        ).images[0]
 
-    image.save("plusface_faceid_with_style.png")
+        save_fname = f"plusface{plusface_scale}_faceid{faceid_scale}_with_style{style_scale}.png"
+        image.save(Path("results", save_fname).as_posix())
 
 else:
     # text2image with 2 image embeddings
@@ -88,22 +112,24 @@ else:
         ],
         image_encoder_folder=None
     )
-    pipeline.set_ip_adapter_scale([0.5, 0.5])
 
     image_embeds_plusface = torch.load(args.plusface)
     image_embeds_faceid = torch.load(args.faceid)
     image_embeds = [image_embeds_plusface[0], image_embeds_faceid[0]]
 
-    image = pipeline(
-        prompt="a woman",
-        ip_adapter_image_embeds=image_embeds,
-        negative_prompt="monochrome, lowres, bad anatomy, worst quality, low quality", 
-        num_inference_steps=50,
-        num_images_per_prompt=1,
-        guidance_scale = 7.5,
-        width=1024,
-        height=1024, 
-        generator=torch.Generator(device="cpu").manual_seed(0)
-    ).images[0]
+    for (plusface_scale, faceid_scale) in itertools.product(plusface_scale_list, faceid_scale_list):
+        pipeline.set_ip_adapter_scale([plusface_scale, faceid_scale])
+        image = pipeline(
+            prompt="a woman",
+            ip_adapter_image_embeds=image_embeds,
+            negative_prompt="monochrome, lowres, bad anatomy, worst quality, low quality", 
+            num_inference_steps=50,
+            num_images_per_prompt=1,
+            guidance_scale = 7.5,
+            width=1024,
+            height=1024, 
+            generator=torch.Generator(device="cpu").manual_seed(0)
+        ).images[0]
 
-    image.save("plusface_faceid.png")
+        save_fname = f"plusface{plusface_scale}_faceid{faceid_scale}.png"
+        image.save(Path("results", save_fname).as_posix())
